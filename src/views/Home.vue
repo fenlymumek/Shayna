cea3e6fc60e866b0412896d9756c4c696f9cdc1e
fenly mumek <fenlimumek232@gmail.com>
2021-03-02 04:41:31 +0700

    first upload

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <ProductShayna />
+    <InstaShayna />
+    <PartnertShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
+import ProductShayna from '@/components/ProductShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PartnertShayna from '@/components/PartnertShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    ProductShayna,
+    InstaShayna,
+    PartnertShayna,
+    FooterShayna
+  }
+}
+</script>
